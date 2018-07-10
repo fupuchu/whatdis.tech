c1cc97e6637f26e23cb5bf45cd54a06e502824d0
Leon Ho <leonhwh@icloud.com>
2018-07-10 09:39:18 +0800

    testing for heroku

--- configs/database.js
+++ configs/database.js
@@ -5,12 +5,30 @@ const category = require('../models/category')
 const user = require ('../models/userMod')
 const secret = require('./secret')
 
-const config = {
+if( process.env.DATABASE_URL ){
+
+  //we need to take apart the url so we can set the appropriate configs
+
+  const params = url.parse(process.env.DATABASE_URL);
+  const auth = params.auth.split(':');
+
+  //make the configs object
+  const config = {
+    user: auth[0],
+    password: auth[1],
+    host: params.hostname,
+    port: params.port,
+    database: params.pathname.split('/')[1],
+    ssl: true
+  }
+} else {
+  const config = {
     user: secret.hideMe.user,
     host: secret.hideMe.host,
     database: secret.hideMe.database,
     port: secret.hideMe.port
   }
+}
 
 const poolConnect = new pg.Pool(config)
 
